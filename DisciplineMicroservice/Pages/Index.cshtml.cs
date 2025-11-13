4b4d1d94ee982528841f3c9a7dace93c0cacd53e
Isabella-12bit <isabella.montes@ucb.edu.bo>
2025-11-13 08:06:10 -0400

    Añadiendo el microservicio

---
+++ DisciplineMicroservice/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DisciplineMicroservice.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ DisciplineMicroservice/Pages/Update.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DisciplineMicroservice.Pages
+{
+    public class UpdateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
