d0942f11da63ec79939fdb59d05546ade893aae8
Isabella-12bit <isabella.montes@ucb.edu.bo>
2025-11-16 18:27:31 -0400

    Ordenado de carpetas

--- DisciplineMicroservice/DisciplineMicroserviceAPI/Controllers/WeatherForecastController.cs
+++ DisciplineMicroservice/DisciplineMicroserviceAPI/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace DisciplineMicroservice.Controllers
+namespace DisciplineMicroservice.DisciplineMicroserviceAPI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
