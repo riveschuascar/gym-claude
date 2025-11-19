6a5670d449e0881a869eb1cbf371b8270e340325
Isabella-12bit <isabella.montes@ucb.edu.bo>
2025-11-19 14:20:46 -0400

    Arreglando errores de duplicación que impedían el build

--- WebUI/WebUI/Common/Result.cs
+++ WebUI/WebUI/Common/Result.cs
@@ -1,20 +1,26 @@
-namespace WebUI.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Result<T>
+namespace ServiceDiscipline.Application.Common
 {
-    public bool IsSuccess { get; }
-    public bool IsFailure => !IsSuccess;
-    public T? Value { get; }
-    public string Error { get; }
-
-    private Result(bool isSuccess, T? value, string error)
+    public class Result<T>
     {
-        IsSuccess = isSuccess;
-        Value = value;
-        Error = error;
+        public bool IsSuccess { get; }
+        public bool IsFailure => !IsSuccess;
+        public T Value { get; }
+        public string Error { get; }
+
+        private Result(bool isSuccess, T value, string error)
+        {
+            IsSuccess = isSuccess;
+            Value = value;
+            Error = error;
+        }
+
+        public static Result<T> Success(T value) => new Result<T>(true, value, string.Empty);
+        public static Result<T> Failure(string error) => new Result<T>(false, default(T), error);
     }
-
-    public static Result<T> Success(T value) => new Result<T>(true, value, string.Empty);
-
-    public static Result<T> Failure(string error) => new Result<T>(false, default, error);
 }
