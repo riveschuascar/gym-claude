b98a2f7b54778fca0be5f38ee5d22008bda00296
Isabella-12bit <isabella.montes@ucb.edu.bo>
2025-11-18 23:24:38 -0400

    Base de datos para probar para el microservicios

---
+++ MembershipMicroservice/sql/MembershipMicroservice.sql
@@ -0,0 +1,20 @@
+﻿--DROP TABLE IF EXISTS membership;
+
+--CREATE TABLE membership (
+--    id SMALLSERIAL PRIMARY KEY,
+--    created_at TIMESTAMP NULL,
+--    last_modification TIMESTAMP NULL,
+--    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+--    name VARCHAR(200) NULL,
+--    price DECIMAL(10,2) NULL,
+--    description TEXT NULL,
+--    monthly_sessions SMALLINT NULL
+--);
+
+--INSERT INTO membership (created_at, name, price, description, monthly_sessions)
+--VALUES
+--(now(), 'Basic Plan', 100.00, 'Acceso limitado al gimnasio.', 12),
+--(now(), 'Premium Plan', 200.00, 'Acceso total y clases ilimitadas.', 30),
+--(now(), 'Student Plan', 80.00, 'Precio especial para estudiantes.', 8);
+
+--SELECT * FROM membership;
